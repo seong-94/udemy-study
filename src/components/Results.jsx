92231cd82f9d08be381cba73125456c780fba8ba
seong-94 <togussla@naver.com>
2024-02-06 23:58:59 +0900

    강의 102

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,6 +1,33 @@
-import { calculateInvestmentResults } from "../util/investment.js";
+import { calculateInvestmentResults, formatter } from "../util/investment.js";
 
 export default function Results({ input }) {
   const userData = calculateInvestmentResults(input);
-  return <>{/* <div>{userData}</div> */}</>;
+  const initialInvestment = userData[0];
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Year</th>
+          <th>Investment Value</th>
+          <th>Interest (Year)</th>
+          <th>total Interest</th>
+          <th>Invested Capital</th>
+        </tr>
+      </thead>
+      <tbody>
+        {userData.map((yearData) => {
+          const totaInterest = yearData.valueEndOfYear - yearData.annualInvestment * yearData.year;
+          return (
+            <tr key={yearData.year}>
+              <td>{yearData.year}</td>
+              <td>{formatter.format(yearData.valueEndOfYear)}</td>
+              <td>{formatter.format(yearData.interest)}</td>
+              <td>{formatter.format(yearData.totaInterest)}</td>
+              <td>{yearData.year}</td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
 }
